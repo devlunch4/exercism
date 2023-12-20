da3eadb48a36dd12702dafce3f0833a42db2e4fc
Michael W Kim <devlunch4@gmail.com>
2023-12-20 04:50:36 -0500

    002_Reverse_String_answer.rs add new code.

--- rust/002_Reverse_String_answer.rs
+++ rust/002_Reverse_String_answer.rs
@@ -2,3 +2,10 @@ pub fn reverse(input: &str) -> String {
    // todo!("Write a function to reverse {input}");
    input.chars().rev().collect()
 }
+
+
+// //new version
+// use unicode_segmentation::UnicodeSegmentation;
+// pub fn reverse(input: &str) -> String {
+//     input.graphemes(true).rev().collect()
+// }
