14ce017b0921348d34a4084046c04970694b74f6
Michael W Kim <devlunch4@gmail.com>
2023-12-20 10:38:33 -0500

    Modified for better.

--- rust/003_Gigasecond_answer.rs
+++ rust/003_Gigasecond_answer.rs
@@ -1,8 +1,13 @@
+// use time::PrimitiveDateTime as DateTime;
+//     use time::Duration;
+// // Returns a DateTime one billion seconds after start.
+// pub fn after(start: DateTime) -> DateTime {
+//     let gigasecond = Duration::seconds(1000000000);
+//     return start+gigasecond;
+// }
 use time::PrimitiveDateTime as DateTime;
 use time::Duration;
-
 // Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
-    let gigasecond = Duration::seconds(1000000000);
-    return start+gigasecond;
+    start.checked_add(Duration::seconds(1000000000 )).unwrap()
 }
